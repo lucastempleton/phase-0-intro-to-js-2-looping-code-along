2397c386e6cb609900bf5c413720691f59726bd3
Lucas Templeton <templetl99@gmail.com>
2021-10-24 22:20:51 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Lisa","Caitlyn","Jan"]
+function writeCards(names){
+    const newArray = []
+    for (let i=0; i<names.length; i++){
+        newArray.push (`Thank You, ${names[i]}, for the wonderful surprise gift!`);
+        
+    }
+    return newArray
+    
+}
+
+
+
+
+function countDown(){
+    for (let count=10; count>=0; count--){
+        console.log(count);
+    }
+}
